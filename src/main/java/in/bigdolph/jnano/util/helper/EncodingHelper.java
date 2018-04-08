d5c94da0a32dc8bf73bb620b1cb834419ced6824
Karl Oczadly <karl@bigdolph.in>
2018-04-08 18:31:14 +0100

    Added encoding helper classes

---
+++ src/main/java/in/bigdolph/jnano/util/helper/EncodingHelper.java
@@ -0,0 +1,9 @@
+package in.bigdolph.jnano.util.helper;
+
+public class EncodingHelper {
+    
+    public static final BaseEncoder HEXADECIMAL = new BaseEncoder("0123456789ABCDEF");
+    
+    public static final BaseEncoder NANO_BASE32 = new BaseEncoder("13456789abcdefghijkmnopqrstuwxyz");
+    
+}
